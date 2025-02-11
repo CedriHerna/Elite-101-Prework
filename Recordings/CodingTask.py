dddc19a24243b3d73d3341f7e94d63a0da3d58d7
CedriHerna <cedricphernandez@gmail.com>
2025-02-11 00:08:48 +0000

    commiting stuff

---
+++ Recordings/CodingTask.py
@@ -0,0 +1,25 @@
+# In this coding task we have to return the indexes of Captial letters in a string
+
+# string = "WasSup"
+# store_index = []
+
+# def capital_indexes(string):
+
+#     for i, charater in enumerate(string):
+        
+#         if(charater.isupper()):
+#             store_index.append(i)
+
+# capital_indexes(string)
+# print(store_index)
+
+
+
+
+
+
+
+
+
+
+
