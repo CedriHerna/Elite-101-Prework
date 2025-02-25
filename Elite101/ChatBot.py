511b0d1647d831b7ec38de04a8cb4e5491cbcb1d
CedriHerna <cedricphernandez@gmail.com>
2025-02-25 04:31:22 +0000

    Finished return item

--- Elite101/ChatBot.py
+++ Elite101/ChatBot.py
@@ -4,6 +4,9 @@ import random
 # This is an array that has the list of clothing items
 clothing_list = ["Shirt", "Shirt", "Skirt", "Pants", "Dress"]
 
+# To return an item, the user needs a list of return locations
+return_hubs = ["Austin", "Round Rock", "Pflugerville", "Georgetown", "Cedar Park"]
+
 clothing_dictionary = {"Shirt" : 4.00, "shorts" : 3.25, "Skirt" : 3.25, "Pants" : 4.00, "Dress" : 6.00}
 
 # This is going to randomize a number and then randomly give the user an item.
@@ -36,7 +39,7 @@ print("\nWould you like to return or exhange an item? Type the number of option 
 # Here are the options that the user can choose from
 # 1. Return - 
 # 2. Exchange - 
-array_options = ["1. Help me return my item.", "2. Help me exhange my item.", "3. Exit the chatbot."]
+array_options = ["1. Help me return my item.", "2. Help me exhange my item.", "3. Confirmation/Exchange Number", "4. Exit the chatbot."]
 
 
 
@@ -50,52 +53,72 @@ def user_choice():
 
     
     if(choice == "1"):
-        reciept = input("Please enter the reciept number to confirm your purchase: ")
+        return_item()
+
     elif(choice == 2):
-        # asdf
-    elif(choice == "3"):
+        print("Fortnite baby")
+    elif(choice == 3):
+        print("Confirmation and exchange stuffs.")
+    elif(choice == "4"):
         print("Have a great day, " + name + "!")
     else:
         print("Return a valid response (Just the number at the start of each option. Eg. \"1\")")
 
 
+def return_item():
+    
+    reciept = input("Please enter the reciept number to confirm your purchase. If you chose the wrong option, type exit. ").lower()
+
+    while(True):   #Looping indefinetly so the user can punch in the right code.
+
+        if(reciept == "exit"):
+            print("Exiting to home page.")
+            return       #Allows me to skip to rest of the code after this while loop and return to the main function.
+        elif(int(reciept) == random_reciept_num):
+            print(f"Thank you for your confirmation. You are trying to return a {random_item}.")
+            break
+        else:
+            reciept = input("Unknown reciept. Please try again. *Enter the receipt number that is at the very top of the terminal* ")
+
+    print(f"The return value for this item is $ {clothing_dictionary[random_item]}")
+    confirmation = input("Do you confirm that you are returning your item for this price? (Yes or No) *Notice: If you type \"No\" you will be sent back to the home page.").lower()
+    
+    while(True):
+
+        if(confirmation == "yes"):
+            break
+        elif(confirmation == "no"):
+            print("\n\n")
+            return
+        else:
+            confirmation = input("Please reenter your response as either \"Yes\" or \"No\".")
+    
+    confirmation_number = random.randint(0,10000)
+    print(f"Wonderful! Please go to any close Perico Clothing hub and show them this confirmation code at the return desk: {confirmation_number}")
+    print("\n\n")
+
+
+def exchange_item():
+
+    reciept = input("Please enter the reciept number to confirm your purchase. If you chose the wrong option, type exit. ").lower()
+
+    while(True):   #Looping indefinetly so the user can punch in the right code.
+
+        if(reciept == "exit"):
+            print("Exiting to home page.")
+            return       #Allows me to skip to rest of the code after this while loop and return to the main function.
+        elif(int(reciept) == random_reciept_num):
+            print(f"Thank you for your confirmation. You are trying to exchange your {random_item} for another item.")
+            break
+        else:
+            reciept = input("Unknown reciept. Please try again. *Enter the receipt number that is at the very top of the terminal* ")
+
+    print("What item would you like to exhange for? You can only exhange for an item less or equal to the return value of your item")
+
+    for i, item in enumerate(clothing_dictionary):
+
+        print(f"{i}, {item}")
+
 
 user_choice()
-
-
-
-
-# for option in array_options:
-#     print(option)
-
-# choice = input("\n")
-
-# if choice == 1,2,3,4
-
-# if(choice == "1"):
-#     # Pull from a dictionary where package has estimated time of delievery and where it currently is
-#     print("What is your package ID?")
-#     print("Your package is at " + dictionary_location + ". It will arrive " + dictionary_date_of_arrival)
-
-# elif(choice == "2"):
-#     # Pull from the total cost of a certain delievery and run a calculation (probably in a method) to get the price breakdown.
-#     print("What is your package ID?")
-#     print("Your price breakdown is...")
-
-# elif(choice == "3"):
-#     # These responses should be a listing of what is being shipped, where it goes, and who gets it. It should also create a random package ID in the end.
-#     shipment = input("What are you shipping?")
-#     end_location = input("Where will this shipment end up?")
-#     recipient = input("Who will be recieving this shipment?")
-#     print("Your created package ID is ...")
-
-# elif(choice == "4"):
-# 	# Should return the package IDs, loaction, total price, and addresses of the shipment.
-#     print("Return the dictionary in a formated manner")
-
-# elif(choice == "5"):
-#     print("Have a great day, " + name + "!")
-# else:
-#     print("Return a valid response (Just the number at the start of each option. Eg. \"1\")")
-
 
